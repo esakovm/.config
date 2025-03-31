9867f06591abaa87dfc6f9a010f704cd5d13c788
Maxim Esakov <esakovm@dtechs.io>
2025-03-31 05:57:24 +0300

    Поменял тему

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
+-- return {
+-- 	{
+-- 		"craftzdog/solarized-osaka.nvim",
+-- 		lazy = true,
+-- 		priority = 1000,
+-- 		opts = function()
+-- 			return {
+-- 				transparent = true,
+-- 			}
+-- 		end,
+-- 	},
+-- }
 return {
+	{ "scottmckendry/cyberdream.nvim" },
+
 	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "cyberdream",
+		},
 	},
 }
